e18356c90e2842c08fa602b287bffa219e0947e2
mariapiachiatante <59983974+mariapiachiatante@users.noreply.github.com>
2020-02-06 19:19:24 +0100

    check

--- R/Script_airlinesafety.R
+++ R/Script_airlinesafety.R
@@ -1,13 +1,18 @@
 #AIRLINES SAFETY ANALYSIS#
+air_safety<-read.csv("https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv", sep=",")
+save(air_safety, file = "C:/Users/chiat/Documents/GitHub/f0e7a855/data/airsafety.rda")
+load("~/GitHub/f0e7a855/data/airsafety.rda")
+
 prepare <- function() {
   #Create a new variable called "Fatal accidents rate", given by the number of
   #fatal accidents in 2000-2014 over the number of incidents in 2000-2014
-  fa_rate<-airlinesafety::air_safety$fatal_accidents_00_14/airlinesafety::air_safety$incidents_00_14
+  fa_rate<-air_safety$fatal_accidents_00_14/air_safety$incidents_00_14
   #Add the new variable to the dataset
-  airlinesafety::air_safety<-cbind(airlinesafety::air_safety,fa_rate) %>%
+  library(tidyverse)
+  air_safety<-cbind(air_safety,fa_rate) %>%
     mutate(safety = fa_rate<0.2)
   #Check the dimensions of the dataset
-  return(airlinesafety::air_safety)
+  return(air_safety)
 }
 
 prepare()
@@ -16,10 +21,10 @@ prepare()
 #of the airline is smaller than 0.2, otherwise it returns FALSE
 fa <- function(){
   library(tidyverse)    #package for the "mutate" function
-  airlinesafety::air_safety<-prepare() %>%
+  air_safety<-prepare() %>%
     mutate(safety = fa_rate<0.2)
   #Check the dimensions of the dataset
-  dim(airlinesafety::air_safety)
+  dim(air_safety)
 }
 
 fa()
@@ -28,8 +33,8 @@ plot1 <- function(x, y, tipe = 'bar', title) {
   #Data Visualization with plotly
   library(plotly)
   #Barplot representing the number of fatal accidents for each airline in the two periods
-  b <- plot_ly(airlinesafety::air_safety, x = ~airlinesafety::air_safety$airline, y = ~airlinesafety::air_safety$fatal_accidents_85_99, type = 'bar', name = '1985-1999') %>%
-    add_trace(y = ~airlinesafety::air_safety$fatal_accidents_00_14, name = '2000-2014') %>%
+  b <- plot_ly(air_safety, x = ~air_safety$airline, y = ~air_safety$fatal_accidents_85_99, type = 'bar', name = '1985-1999') %>%
+    add_trace(y = ~air_safety$fatal_accidents_00_14, name = '2000-2014') %>%
     layout(yaxis = list(title = 'Number of Fatal Accidents'), barmode = 'Airline')
   return(b)
 }
@@ -50,18 +55,18 @@ correlation <- function(){
   #Install and load the package corrplot
   library(corrplot)
   #Correlation between the numeric variables in the dataset
-  numeric.var<-sapply(airlinesafety::air_safety,is.numeric)
-  corr.matrix<-cor(airlinesafety::air_safety[,numeric.var])
+  numeric.var<-sapply(air_safety,is.numeric)
+  corr.matrix<-cor(air_safety[,numeric.var])
   corrplot(corr.matrix,main="\n\nCorrelation Matrix",method="number")
 }
 correlation()
 
 reg <- function(){
   #Simple Linear Regression
-  reg_1<-lm(air_safety$fatal_accidents_00_14~air_safety$incidents_00_14, data=airlinesafety::air_safety)
+  reg_1<-lm(air_safety$fatal_accidents_00_14~air_safety$incidents_00_14, data=air_safety)
   summary(reg_1)
   #Multiple Linear Regressions
-  reg_2<-lm(air_safety$fatal_accidents_00_14~air_safety$incidents_00_14+air_safety$fatalities_00_14, data=airlinesafety::air_safety)
+  reg_2<-lm(air_safety$fatal_accidents_00_14~air_safety$incidents_00_14+air_safety$fatalities_00_14, data=air_safety)
   summary(reg_2)
   result<-list(summary(reg_1), summary(reg_2))
   return(result)
